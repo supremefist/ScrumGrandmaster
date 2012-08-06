01007a956fe5a51ddb8400a6492c80bddbe07c81
supremefist <sakkie99@gmail.com>
2012-08-06 10:40:33 +0200

    Initial project

---
+++ src/com/supremefist/scrumgm/ScrumGM.java
@@ -0,0 +1,13 @@
+package com.supremefist.scrumgm;
+
+public class ScrumGM {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Welcome!");
+    }
+
+}
